30c4947279c97496d82372e88df77afa2030dd19
ParhamTny <parham.taherkhanii@gmail.com>
2024-12-21 00:18:22 +0330

    Use Instance

--- Resurrect/Source/Main.cpp
+++ Resurrect/Source/Main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
 	Resurrect::Window window("Resurrect window", { 800, 600 }, { 100, 100 });
+	Resurrect::Instance instance("Resurrect application", { 1, 0 });
 	while (!window.ShouldClose());
 
 	return 0;
